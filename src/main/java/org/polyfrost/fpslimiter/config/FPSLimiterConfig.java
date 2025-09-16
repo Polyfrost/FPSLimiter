98e9f55684fe54a0f863532358aef3b6f57ae210
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-09-16 10:55:07 +0200

    unfocused sounds

--- src/main/java/org/polyfrost/fpslimiter/config/FPSLimiterConfig.java
+++ src/main/java/org/polyfrost/fpslimiter/config/FPSLimiterConfig.java
@@ -4,10 +4,9 @@ import org.polyfrost.fpslimiter.FPSLimiter;
 import org.polyfrost.oneconfig.api.config.v1.Config;
 import org.polyfrost.oneconfig.api.config.v1.annotations.Checkbox;
 import org.polyfrost.oneconfig.api.config.v1.annotations.Number;
+import org.polyfrost.oneconfig.api.config.v1.annotations.Slider;
 
 public class FPSLimiterConfig extends Config {
-    // todo: unfocused sound volume
-
     @Checkbox(
             title = "Use Custom FPS Limit"
     )
@@ -24,10 +23,10 @@ public class FPSLimiterConfig extends Config {
     )
     public boolean useMaxMainMenuFpsLimit = false;
 
-    @Number(
+    @Slider(
             title = "Max Main Menu FPS Limit",
             description = "Seems to not work :(",
-            min = 5f, max = 255f
+            min = 5f, max = 255f, step = 5f
     )
     public int maxMainMenuFpsLimit = 30;
 
@@ -41,6 +40,12 @@ public class FPSLimiterConfig extends Config {
             min = 5f, max = 255f
     )
     public int unfocusedFpsLimit = 60;
+
+    @Slider( // todo: number inputs seem to be broken
+            title = "Unfocused Sounds",
+            min = 0f, max = 1f, step = 0.05f
+    )
+    public float unfocusedSounds = 1f;
 
     public FPSLimiterConfig() {
         super(FPSLimiter.ID + ".json", FPSLimiter.NAME, Category.QOL);
